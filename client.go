040298e77afec75af289b4a30756e4b0ba1f13e8
tpmuniz <thiagopermuniz@gmail.com>
2024-09-25 18:57:30 -0300

    encapsulated methods

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"net/http"
+	"net/url"
 )
 
 func NewHttpClient(baseURL string, opts ...ClientOption) *ClientConfig {
@@ -17,40 +18,55 @@ func NewHttpClient(baseURL string, opts ...ClientOption) *ClientConfig {
 	return client
 }
 
-func (c *ClientConfig) Get(ctx context.Context, endpoint string, reqConfig *RequestConfig) (*http.Response, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+endpoint, nil)
+// prepareRequest prepara uma requisição HTTP com base nas configurações fornecidas.
+func (c *ClientConfig) prepareRequest(ctx context.Context, method, endpoint string, reqConfig *RequestConfig) (*http.Request, error) {
+	// Cria a URL completa
+	fullURL, err := url.Parse(c.baseURL + endpoint)
 	if err != nil {
 		return nil, err
 	}
 
+	// Cria a requisição com o método e o corpo (se houver)
+	var body *bytes.Buffer
+	if reqConfig.body != nil {
+		body = bytes.NewBuffer(reqConfig.body)
+	} else {
+		body = nil
+	}
+	req, err := http.NewRequestWithContext(ctx, method, fullURL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Adiciona os cabeçalhos
 	for key, header := range reqConfig.headers {
 		req.Header.Set(key, header)
 	}
 
+	// Adiciona os parâmetros de query string
 	q := req.URL.Query()
 	for key, value := range reqConfig.query {
 		q.Add(key, value)
 	}
 	req.URL.RawQuery = q.Encode()
 
+	return req, nil
+}
+
+// Get faz uma requisição HTTP GET para o endpoint especificado.
+func (c *ClientConfig) Get(ctx context.Context, endpoint string, reqConfig *RequestConfig) (*http.Response, error) {
+	req, err := c.prepareRequest(ctx, http.MethodGet, endpoint, reqConfig)
+	if err != nil {
+		return nil, err
+	}
 	return c.httpClient.Do(req)
 }
 
-func (c *ClientConfig) Post(ctx context.Context, reqConfig *RequestConfig) (*http.Response, error) {
-	req, err := http.NewRequestWithContext(ctx, "POST", c.baseURL, bytes.NewBuffer(reqConfig.body))
+// Post faz uma requisição HTTP POST para o endpoint especificado.
+func (c *ClientConfig) Post(ctx context.Context, endpoint string, reqConfig *RequestConfig) (*http.Response, error) {
+	req, err := c.prepareRequest(ctx, http.MethodPost, endpoint, reqConfig)
 	if err != nil {
 		return nil, err
 	}
-
-	for key, header := range reqConfig.headers {
-		req.Header.Set(key, header)
-	}
-
-	q := req.URL.Query()
-	for key, value := range reqConfig.query {
-		q.Add(key, value)
-	}
-	req.URL.RawQuery = q.Encode()
-
 	return c.httpClient.Do(req)
 }
